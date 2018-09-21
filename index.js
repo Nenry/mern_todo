71448155243deed8c709c6fc2dba9ab70b871aa0
HN <hhnguyen92@gmail.com>
2018-09-20 17:13:39 -0700

    Add graphQL server and mongoose settings

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-import {
-  GraphQLServer
-} from 'graphql-yoga'
-// ... or using `require()`
-// const { GraphQLServer } = require('graphql-yoga')
+
+const { GraphQLServer } = require('graphql-yoga');
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/test5");
+//known as schema in graphql
+//name is name of argument, String is the data type. String with ! is mandatory
 
 const typeDefs = `
   type Query {
     hello(name: String): String!
   }
-`
+`;
 
+//destructuring name and => returns response
 const resolvers = {
   Query: {
     hello: (_, {
@@ -21,5 +24,8 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs,
   resolvers
-})
-server.start(() => console.log('Server is running on localhost:4000')
+});
+
+mongoose.connect.once("open", function(){
+  server.start(() => console.log('Server is running on localhost:4000'));
+});
